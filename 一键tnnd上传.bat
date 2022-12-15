53a2ec372b5797c5b258d6a54c88569fec12020e
zhiyiyi <1187303661@qq.com>
2022-12-15 15:57:40 +0800

    push now!

--- 一键tnnd上传.bat
+++ 一键tnnd上传.bat
@@ -1,15 +1,23 @@
 @echo off
+
+::一些没啥用的设置
+title fukin push
+
+:backhome
+
 echo =================
 echo =  welcome  =
-echo 
+echo = 1.asd (upload your files immediately)
+echo = 2.qwe (show the details of this program)
 echo =================
-:backhome
-set /p inp=input "asd" and I will upload al your files:
-if "%inp%" equ "asd" （
+
+set /p inp=input your choice:
+if %inp% == asd （
 git add .
 git commit -m "push now!"
 git push
 cls
 )
+
 goto backhome
 pause

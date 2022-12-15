7ea757c9e9e419eedbe9d7ac8725ba86ec74b324
zhiyiyi <1187303661@qq.com>
2022-12-15 15:52:20 +0800

    push now!

--- 一键tnnd上传.bat
+++ 一键tnnd上传.bat
@@ -5,10 +5,11 @@ echo
 echo =================
 :backhome
 set /p inp=input "asd" and I will upload al your files:
-if %inp% equ "asd" （
+if "%inp%" equ "asd" （
 git add .
 git commit -m "push now!"
 git push
+cls
 )
 goto backhome
 pause
